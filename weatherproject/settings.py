c9e044e99f0304c8fff2d9f16f66f313b50522d5
谷口純也 <taniguchi@taniguchijunya-no-iMac.local>
2018-10-16 11:39:57 +0900

    BootStrap追加

--- weatherproject/settings.py
+++ weatherproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x7#9k47=#ngc(y6#e%v2is1c0m1fn=epm)fewsag_i^d4i4h3c'
+SECRET_KEY = 'your secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- weatherproject/views.py
+++ weatherproject/views.py
@@ -3,20 +3,23 @@ from django.shortcuts import render
 import requests
 import json
 
+
 def home(request):
     return render(request, 'home.html')
 
-def ask(request):
-    fulltext = request.GET['fulltext']
-    city = fulltext
-    APIkey = "6006c8a1d90dd8e603fc35057667698f"
 
-    api = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&APPID="+APIkey
+def ask(request):
+    city = request.GET['city']
+    city = city
+    APIkey = "your APIkey"
+
+    api = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + APIkey
     r = requests.get(api)
     data = json.loads(r.text)
 
-    k2c = lambda k: k - 273.15
+    def k2c(k): return k - 273.15
     name = data["name"]
     weather = data["weather"][0]["description"]
-    ondo = k2c(data["main"]["temp_min"])
-    return render(request, 'ask.html' ,{'name' : name,'weather': weather,'ondo': ondo})
+    temp_max = k2c(data["main"]["temp_max"])
+    temp_min = k2c(data["main"]["temp_min"])
+    return render(request, 'ask.html', {'name': name, 'weather': weather, 'temp_max': temp_max, 'temp_min': temp_min})
